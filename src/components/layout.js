f9e4b9c6eb9afe81fc78fa6d28c510e34be25e8e
jake <jaggre@live.com>
2020-08-21 19:18:36 -0700

    uhh

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,8 +9,11 @@ import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
+import "semantic-ui-less/semantic.less";
+
 import Header from "./header"
 import "./layout.css"
+
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
--- src/pages/index.js
+++ src/pages/index.js
@@ -12,6 +12,8 @@ const IndexPage = () => (
 
     <iframe src="https://public.tableau.com/views/ACNHVillagersbyPopularitywithfilters/AnimalCrossingVillagersbyPopularity?:showVizHome=no&:embed=true"
  width="100%" height="800" title="Animal Crossing Villagers by Popularity"></iframe>
+
+ <button href="https://forms.gle/S8Lj9zTLpNjLY7B7A"></button>
     
   </Layout>
 )

52995c6fa6c42430d34bc5de9c6d97514af14b13
jake <jaggre@live.com>
2020-08-22 10:21:12 -0700

    new style file
    new style file

--- src/components/header.js
+++ src/components/header.js
@@ -2,6 +2,8 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
+import "./style.css"
+
 const Header = ({ siteTitle }) => (
   <header
     style={{
@@ -9,6 +11,7 @@ const Header = ({ siteTitle }) => (
       marginBottom: `1.45rem`,
     }}
   >
+
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous"></link>
  
 

728ccf0266b3d9a9df9263e993ce9da63c37b8d3
jake <jaggre@live.com>
2020-08-21 23:44:28 -0700

    browswer

--- src/components/header.js
+++ src/components/header.js
@@ -16,6 +16,18 @@ const Header = ({ siteTitle }) => (
   crossorigin="anonymous"
 />
 
+<script src="https://unpkg.com/react/umd/react.production.min.js" crossorigin></script>
+
+<script
+  src="https://unpkg.com/react-dom/umd/react-dom.production.min.js"
+  crossorigin></script>
+
+<script
+  src="https://unpkg.com/react-bootstrap@next/dist/react-bootstrap.min.js"
+  crossorigin></script>
+
+<script>var Alert = ReactBootstrap.Alert;</script>
+
 
 <Navbar bg='rgb(124, 201, 195)' variant="dark">
     <Navbar.Brand href="#home"><img src="https://i.imgur.com/eGqetql.png"></img></Navbar.Brand>

832da0a39df24b4e2cd2aad11a51910d620273a5
jake <jaggre@live.com>
2020-08-21 23:39:39 -0700

    new nav

--- src/components/header.js
+++ src/components/header.js
@@ -16,22 +16,15 @@ const Header = ({ siteTitle }) => (
   crossorigin="anonymous"
 />
 
-<nav class="navbar navbar-expand-md navbar-light bg-dark">
-  <a class="navbar-brand" href="#"><img src="https://i.imgur.com/eGqetql.png"></img></a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="https://forms.gle/S8Lj9zTLpNjLY7B7A">Submit Your Villagers</a>
-      </li>
-    </ul>
-  </div>
-</nav>
+
+<Navbar bg="dark" variant="dark">
+    <Navbar.Brand href="#home"><img src="https://i.imgur.com/eGqetql.png"></img></Navbar.Brand>
+    <Nav className="mr-auto">
+      <Nav.Link href="#">Home</Nav.Link>
+      <Nav.Link href="https://forms.gle/S8Lj9zTLpNjLY7B7A">Submit Your Villagers</Nav.Link>
+    </Nav>
+  </Navbar>
+
   
 
 <div

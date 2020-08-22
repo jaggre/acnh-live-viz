928dc5e2aca34ed6ab7103b44c144bbfa8d773fa
jake <jaggre@live.com>
2020-08-21 17:33:01 -0700

    initial commit
    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Animal Crossing Villagers by Popularity`,
+    description: `A reactive guide made by two idiots`,
+    author: `Dash Foster, Jake Rusnak`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: 'rgb(124, 201, 195)',
       marginBottom: `1.45rem`,
     }}
   >

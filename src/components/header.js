ac3b249d49c547a53af3c59e8ebf5db37c889850
jake <jaggre@live.com>
2020-08-21 23:53:09 -0700

    even better nav

--- src/components/header.js
+++ src/components/header.js
@@ -31,10 +31,6 @@ const Header = ({ siteTitle }) => (
       <a class="nav-link" href="https://forms.gle/S8Lj9zTLpNjLY7B7A">Submit Your Villagers</a>
       </li>
     </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
   </div>
 </nav>
   

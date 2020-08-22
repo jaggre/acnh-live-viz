065437dea9875cf07bd86839f9a38593b8fcfd12
jake <jaggre@live.com>
2020-08-21 22:04:03 -0700

    dash graph

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,9 @@ const IndexPage = () => (
     <SEO title="Home" />
       <title>Animal Crossing Villagers by Popularity</title>
 
-    <iframe src="https://public.tableau.com/views/ACNHVillagersbyPopularitywithfilters/AnimalCrossingVillagersbyPopularity?:showVizHome=no&:embed=true"
+    <iframe src="https://public.tableau.com/views/ACNHVillagersbyPopularitywithfilters/AnimalCrossingVillagersbyPopularity?"
+"https://public.tableau.com/views/VillagerInfoandoftotalcount/Sheet1?:showVizHome=no&:embed=true"
+    
  width="100%" height="800" title="Animal Crossing Villagers by Popularity"></iframe>
   <div>
  <a href="https://forms.gle/S8Lj9zTLpNjLY7B7A"><button class="ui blue button">Click Here to Contribute</button></a>

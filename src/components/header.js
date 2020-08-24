89ba1e5997c60036a91e50c30af24808ebfce259
jake <jaggre@live.com>
2020-08-23 18:54:04 -0700

    having trouble with local images

--- src/components/header.js
+++ src/components/header.js
@@ -16,7 +16,7 @@ const Header = ({ siteTitle }) => (
  
 
   <nav class="navbar  navbar-dark bg-acnh">
-<a class="navbar-brand" href="#"><img src={`hazel-icon.png`}></img></a>
+<a class="navbar-brand" href="#"><img src='https://i.imgur.com/yQKnH9C.png'></img></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>

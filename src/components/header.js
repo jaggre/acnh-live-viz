ad8f4b3d58affafe7573247b98035c1518fdc59b
jake <jaggre@live.com>
2020-08-23 18:37:04 -0700

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -15,7 +15,7 @@ const Header = ({ siteTitle }) => (
  
 
   <nav class="navbar  navbar-dark bg-acnh">
-<a class="navbar-brand" href="#"><img src="images/hazel-icon.png"></img></a>
+<a class="navbar-brand" href="#"><img src="./images/hazel-icon.png"></img></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>

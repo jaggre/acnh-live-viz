639a2e89a5f3a3e2221e5ac0f843f60b5482c8f9
dash910 <70075134+dash910@users.noreply.github.com>
2020-09-02 15:21:43 -0700

    about section
    added text to the new about section

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,8 +7,12 @@ import SEO from "../components/seo"
 const SecondPage = () => (
   <Layout>
     <SEO title="about" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
+    <h1>About</h1>
+    <p>acnh.pro started out with the question “What villagers are the most common on player’s islands?”. To answer this, we made a poll for New Horizons players to fill out asking them about their current villagers. Then, we displayed the results in a fun and easy to view format that lets you filter by species, personality, and live count of poll responses. 
+</p>
+<p>Since the initial poll, acnh.pro has grown to be a helpful resource for New Horizons players trying to view data mine information in an easy to access format. 
+</p>
+<p>We’d like to thank everyone who responded to the poll and helped us create the data. Results are updated frequently so tell your friends to answer the poll and keep enjoying the island life!</p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )

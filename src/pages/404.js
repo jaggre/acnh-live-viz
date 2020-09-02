3de68445caa7fd1807ae1be669f441271781be72
jake <jaggre@live.com>
2020-09-02 14:32:24 -0700

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -7,7 +7,8 @@ const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
     <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <img src="blob:https://imgur.com/7ca1b92a-6909-46cb-bd30-8ad3eb8c9860"></img>
+    <p>You seem to have taken a wrong turn...</p>
   </Layout>
 )
 

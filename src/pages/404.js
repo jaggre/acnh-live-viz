c4308474db6b4b95f799e28ded9c75491bea17f8
jake <jaggre@live.com>
2020-09-02 14:39:55 -0700

    styling, 404
    updating styling

--- src/pages/404.js
+++ src/pages/404.js
@@ -7,7 +7,7 @@ const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
     <h1>NOT FOUND</h1>
-    <img src="https://i.imgur.com/JxKhsHy.png"></img>
+    <img class="#lost-img" src="https://i.imgur.com/JxKhsHy.png"></img>
     <p>You seem to have taken a wrong turn...</p>
   </Layout>
 )

a167c9823e6427179ec3ca4ba56826c6d34548dd
jake <jaggre@live.com>
2020-12-14 08:55:17 -0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,7 +36,7 @@ module.exports = {
           // The property ID; the tracking code won't be generated without it
           trackingId: "G-1NCMJ750VM",
           // Defines where to place the tracking script - `true` in the head and `false` in the body
-          head: false,
+          head: true,
           // Setting this parameter is optional
           anonymize: true,
           // Setting this parameter is also optional
@@ -56,7 +56,7 @@ module.exports = {
           // Any additional optional fields
           sampleRate: 5,
           siteSpeedSampleRate: 10,
-          cookieDomain: "example.com",
+          cookieDomain: "acnh.pro",
         },
       },
     
